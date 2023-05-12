d558072d498523e6f61f94b2ce1df3a591e99bc9
pierrefrancoispaoletti <pierre.paoletti1@gmail.com>
2023-05-12 18:48:51 +0200

    commit to push on main

--- src/_const.js
+++ src/_const.js
@@ -1,6 +1,6 @@
 export const placeLocation = "illy";
 export let serverURI =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV === "development"
     ? "http://localhost:8080"
     : "https://young-badlands-89715.herokuapp.com";
 
